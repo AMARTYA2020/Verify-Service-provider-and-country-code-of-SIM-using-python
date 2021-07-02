41e81a76b24df736f3f9d37fa40e22f74eb0c20d
Amartya pandey <amartyapandey19@gmail.com>
2021-07-02 11:33:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import phonenumbers
+from letstest import no
+
+from phonenumbers import geocoder
+ch = phonenumbers.parse(no,"CH")  #C=country H= history
+print(geocoder.description_for_number(ch, "en"))
+
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(no, "RO")
+print(carrier.name_for_number(service_provider, "en"))
