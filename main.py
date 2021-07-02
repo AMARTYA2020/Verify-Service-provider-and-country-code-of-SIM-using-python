fb048540956c5595b2b959af50321da998b51a17
Amartya pandey <amartyapandey19@gmail.com>
2021-07-02 11:34:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from phonenumbers import geocoder
 ch = phonenumbers.parse(no,"CH")  #C=country H= history
 print(geocoder.description_for_number(ch, "en"))
 
+
 from phonenumbers import carrier
 service_provider = phonenumbers.parse(no, "RO")
 print(carrier.name_for_number(service_provider, "en"))
