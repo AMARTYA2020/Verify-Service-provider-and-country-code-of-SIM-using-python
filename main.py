6cbf24761297e982bfa2fd7fbf3dfec0c4f94526
Amartya pandey <amartyapandey19@gmail.com>
2021-07-02 11:34:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ print(geocoder.description_for_number(ch, "en"))
 from phonenumbers import carrier
 service_provider = phonenumbers.parse(no, "RO")
 print(carrier.name_for_number(service_provider, "en"))
+
+# To get 
